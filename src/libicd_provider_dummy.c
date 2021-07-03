b126c34e8b095fa8a47a592adb556f7a0f8169bb
Merlijn Wajer <merlijn@wizzup.org>
2021-07-03 23:35:00 +0200

    libicd_provider_dummy.c: indent -linux

--- src/libicd_provider_dummy.c
+++ src/libicd_provider_dummy.c
@@ -28,18 +28,16 @@
 #include <support/icd_log.h>
 #include <srv_provider_api.h>
 
-
 #define DUMMY_NETWORK_TYPE "DUMMY"
 #define DUMMY_PROVIDER_TYPE "DUMMY"
 #define DUMMY_PROVIDER_NAME "Dummy Provider"
 #define DUMMY_PROVIDER_ID "dummy-provider"
 
-gboolean icd_srv_init (struct icd_srv_api *srv_api,
-                     icd_srv_watch_pid_fn watch_cb,
-                     gpointer watch_cb_token,
-                     icd_srv_close_fn close,
-                     icd_srv_limited_conn_fn limited_conn);
-
+gboolean icd_srv_init(struct icd_srv_api *srv_api,
+		      icd_srv_watch_pid_fn watch_cb,
+		      gpointer watch_cb_token,
+		      icd_srv_close_fn close,
+		      icd_srv_limited_conn_fn limited_conn);
 
 /**
  * Function to connect (or authenticate) to the service provider.
@@ -56,20 +54,19 @@ gboolean icd_srv_init (struct icd_srv_api *srv_api,
  * @param connect_cb_token  token to pass to the callback
  * @param private           reference to the private icd_srv_api member
  */
-static void dummy_connect (const gchar *service_type,
-                    const guint service_attrs,
-                    const gchar *service_id,
-                    const gchar *network_type,
-                    const guint network_attrs,
-                    const gchar *network_id,
-                    const gchar *interface_name,
-                    icd_srv_connect_cb_fn connect_cb,
-                    gpointer connect_cb_token,
-                    gpointer *private)
+static void dummy_connect(const gchar * service_type,
+			  const guint service_attrs,
+			  const gchar * service_id,
+			  const gchar * network_type,
+			  const guint network_attrs,
+			  const gchar * network_id,
+			  const gchar * interface_name,
+			  icd_srv_connect_cb_fn connect_cb,
+			  gpointer connect_cb_token, gpointer * private)
 {
-    ILOG_DEBUG("dummy_connect: %s\n", network_id);
-    connect_cb(ICD_SRV_SUCCESS, NULL, connect_cb_token);
-    return;
+	ILOG_DEBUG("dummy_connect: %s\n", network_id);
+	connect_cb(ICD_SRV_SUCCESS, NULL, connect_cb_token);
+	return;
 }
 
 /**
@@ -87,22 +84,20 @@ static void dummy_connect (const gchar *service_type,
  * @param disconnect_cb_token  token to pass to the callback
  * @param private              reference to the private icd_srv_api member
  */
-static void dummy_disconnect (const gchar *service_type,
-                       const guint service_attrs,
-                       const gchar *service_id,
-                       const gchar *network_type,
-                       const guint network_attrs,
-                       const gchar *network_id,
-                       const gchar *interface_name,
-                       icd_srv_disconnect_cb_fn disconnect_cb,
-                       gpointer disconnect_cb_token,
-                       gpointer *private)
+static void dummy_disconnect(const gchar * service_type,
+			     const guint service_attrs,
+			     const gchar * service_id,
+			     const gchar * network_type,
+			     const guint network_attrs,
+			     const gchar * network_id,
+			     const gchar * interface_name,
+			     icd_srv_disconnect_cb_fn disconnect_cb,
+			     gpointer disconnect_cb_token, gpointer * private)
 {
-    ILOG_DEBUG("dummy_disconnect: %s\n", network_id);
-    disconnect_cb(ICD_SRV_SUCCESS, disconnect_cb_token);
-    return;
+	ILOG_DEBUG("dummy_disconnect: %s\n", network_id);
+	disconnect_cb(ICD_SRV_SUCCESS, disconnect_cb_token);
+	return;
 }
-
 
 /* Dummy service provider function to identify if a scan result is usable by the
  * provider.
@@ -119,79 +114,71 @@ static void dummy_disconnect (const gchar *service_type,
  *                           been done
  * @param identify_cb_token  token to pass to the identification callback
  */
-static void dummy_identify (enum icd_scan_status status,
-                     const gchar *network_type,
-                     const gchar *network_name,
-                     const guint network_attrs,
-                     const gchar *network_id,
-                     const guint network_priority,
-                     enum icd_nw_levels signal,
-                     const gchar *station_id,
-                     const gint dB,
-                     icd_srv_identify_cb_fn identify_cb,
-                     gpointer identify_cb_token,
-                     gpointer *private) {
-    ILOG_DEBUG("dummy_identify: network_type: %s, network_name: %s, network_id: %s\n", network_type, network_name, network_id);
+static void dummy_identify(enum icd_scan_status status,
+			   const gchar * network_type,
+			   const gchar * network_name,
+			   const guint network_attrs,
+			   const gchar * network_id,
+			   const guint network_priority,
+			   enum icd_nw_levels signal,
+			   const gchar * station_id,
+			   const gint dB,
+			   icd_srv_identify_cb_fn identify_cb,
+			   gpointer identify_cb_token, gpointer * private)
+{
+	ILOG_DEBUG
+	    ("dummy_identify: network_type: %s, network_name: %s, network_id: %s\n",
+	     network_type, network_name, network_id);
 
-    GConfClient *gconf_client;
-    GConfValue *value;
-    GError *error = NULL;
-    gchar* iap_gconf_key;
-    const char* gconf_service_type = NULL;
+	GConfClient *gconf_client;
+	GConfValue *value;
+	GError *error = NULL;
+	gchar *iap_gconf_key;
+	const char *gconf_service_type = NULL;
 
-    gconf_client = gconf_client_get_default();
+	gconf_client = gconf_client_get_default();
 
-    // TODO: we don't read service_id yet
-    // TODO: check this code for memleaks
-    iap_gconf_key = g_strdup_printf("/system/osso/connectivity/IAP/%s/service_type", network_id);
-    value = gconf_client_get(gconf_client, iap_gconf_key, &error);
-    g_free(iap_gconf_key);
+	// TODO: we don't read service_id yet
+	// TODO: check this code for memleaks
+	iap_gconf_key =
+	    g_strdup_printf("/system/osso/connectivity/IAP/%s/service_type",
+			    network_id);
+	value = gconf_client_get(gconf_client, iap_gconf_key, &error);
+	g_free(iap_gconf_key);
 
-    if (error) {
-        g_clear_error(&error);
-    } else {
-        gconf_service_type = gconf_value_get_string(value);
-    }
+	if (error) {
+		g_clear_error(&error);
+	} else {
+		gconf_service_type = gconf_value_get_string(value);
+	}
 
-    g_object_unref(gconf_client);
+	g_object_unref(gconf_client);
 
+	/* We construct a name here to make it apparent this is a dummy provider */
+	gchar *name =
+	    g_strconcat(network_name, " (", DUMMY_PROVIDER_NAME, ") ", NULL);
+	ILOG_DEBUG("dummy_identify: called for: %s\n", name);
 
-    /* We construct a name here to make it apparent this is a dummy provider */
-    gchar *name = g_strconcat(network_name, " (", DUMMY_PROVIDER_NAME, ") ", NULL);
-    ILOG_DEBUG("dummy_identify: called for: %s\n", name);
+	if (g_strcmp0(DUMMY_PROVIDER_NAME, gconf_service_type) == 0) {
+		ILOG_DEBUG("dummy_identify: MATCH\n");
+		identify_cb(ICD_SRV_IDENTIFIED, DUMMY_PROVIDER_TYPE,	/* service type */
+			    name, 0,	/* XXX: service attributes */
+			    DUMMY_PROVIDER_ID, 0,	/* XXX: service priority */
+			    network_type,
+			    network_attrs, network_id, identify_cb_token);
 
-    if (g_strcmp0(DUMMY_PROVIDER_NAME, gconf_service_type) == 0)
-    {
-        ILOG_DEBUG("dummy_identify: MATCH\n");
-        identify_cb(ICD_SRV_IDENTIFIED,
-                    DUMMY_PROVIDER_TYPE, /* service type */
-                    name,
-                    0, /* XXX: service attributes */
-                    DUMMY_PROVIDER_ID,
-                    0, /* XXX: service priority */
-                    network_type,
-                    network_attrs,
-                    network_id,
-                    identify_cb_token);
+	} else {
+		ILOG_DEBUG("dummy_identify: NO MATCH\n");
+		identify_cb(ICD_SRV_UNKNOWN, DUMMY_PROVIDER_TYPE,	/* service type */
+			    name, 0,	/* XXX: service attributes */
+			    DUMMY_PROVIDER_ID, 0,	/* XXX: service priority */
+			    network_type,
+			    network_attrs, network_id, identify_cb_token);
+	}
 
-    } else {
-        ILOG_DEBUG("dummy_identify: NO MATCH\n");
-        identify_cb(ICD_SRV_UNKNOWN,
-                    DUMMY_PROVIDER_TYPE, /* service type */
-                    name,
-                    0, /* XXX: service attributes */
-                    DUMMY_PROVIDER_ID,
-                    0, /* XXX: service priority */
-                    network_type,
-                    network_attrs,
-                    network_id,
-                    identify_cb_token);
-    }
-
-    free(name);
-    return;
+	free(name);
+	return;
 }
-
 
 /**
  * Function to handle child process termination
@@ -200,25 +187,23 @@ static void dummy_identify (enum icd_scan_status status,
  * @param exit_value  process exit value
  * @param private     a reference to the icd_nw_api private member
  */
-static void dummy_child_exit (const pid_t pid,
-                       const gint exit_status,
-                       gpointer *private) {
-    ILOG_DEBUG("dummy_child_exit\n");
-    return;
+static void dummy_child_exit(const pid_t pid,
+			     const gint exit_status, gpointer * private)
+{
+	ILOG_DEBUG("dummy_child_exit\n");
+	return;
 }
-
 
 /**
  * Function to handle service provider destruction
  *
  * @param private  a reference to the icd_nw_api private member
  */
-static void dummy_srv_destruct (gpointer *private)
+static void dummy_srv_destruct(gpointer * private)
 {
-    ILOG_DEBUG("dummy_srv_destruct\n");
-    return;
+	ILOG_DEBUG("dummy_srv_destruct\n");
+	return;
 }
-
 
 /** Dummy service provider module initialization function.
  * @param srv_api icd_srv_api structure filled in by the module
@@ -231,21 +216,21 @@ static void dummy_srv_destruct (gpointer *private)
  *        providing purposes. (optional)
  * @return TRUE on succes; FALSE on failure whereby the module is unloaded
  */
-gboolean icd_srv_init (struct icd_srv_api *srv_api,
-                     icd_srv_watch_pid_fn watch_cb,
-                     gpointer watch_cb_token,
-                     icd_srv_close_fn close,
-                     icd_srv_limited_conn_fn limited_conn)
+gboolean icd_srv_init(struct icd_srv_api * srv_api,
+		      icd_srv_watch_pid_fn watch_cb,
+		      gpointer watch_cb_token,
+		      icd_srv_close_fn close,
+		      icd_srv_limited_conn_fn limited_conn)
 {
-  ILOG_DEBUG("icd_srv_init\n");
+	ILOG_DEBUG("icd_srv_init\n");
 
-  srv_api->version = ICD_SRV_MODULE_VERSION;
-  srv_api->private = NULL;
-  srv_api->connect = dummy_connect;
-  srv_api->disconnect = dummy_disconnect;
-  srv_api->identify = dummy_identify;
-  srv_api->child_exit = dummy_child_exit;
-  srv_api->srv_destruct = dummy_srv_destruct;
+	srv_api->version = ICD_SRV_MODULE_VERSION;
+	srv_api->private = NULL;
+	srv_api->connect = dummy_connect;
+	srv_api->disconnect = dummy_disconnect;
+	srv_api->identify = dummy_identify;
+	srv_api->child_exit = dummy_child_exit;
+	srv_api->srv_destruct = dummy_srv_destruct;
 
-  return TRUE;
+	return TRUE;
 }
